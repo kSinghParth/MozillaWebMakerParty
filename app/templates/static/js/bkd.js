f8fc7d198c7b3d08b7bbdd9c8426b3031a514bc6
Kunwar Parth Singh <parthsingh287@icloud.com>
2017-11-04 04:40:28 +0530

    aas

--- app/templates/static/js/bkd.js
+++ app/templates/static/js/bkd.js
@@ -4,6 +4,8 @@
 				    document.getElementById("main").style.marginLeft = 
 				    "250px";
 				    
+				    document.getElementById("circle-centre").style.z-index="-1";	
+			    
 				}
 
 				function closeNav() {
