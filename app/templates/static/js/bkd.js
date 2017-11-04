26f5f52e0021f57c426f53568425a3416a7f12c2
Kunwar Parth Singh <parthsingh287@icloud.com>
2017-11-04 05:46:54 +0530

    Adding portfolios

--- app/templates/static/js/bkd.js
+++ app/templates/static/js/bkd.js
@@ -13,3 +13,9 @@
 				    document.getElementById("main").style.marginLeft= "0";
 						document.getElementById("circle-centre").style.zIndex="0";	
 				}
+function openNav2(){
+	document.getElementById("blurry").style.zIndex="4";
+}
+function closeNav2(){
+	document.getElementById("blurry").style.zIndex="-1";
+}
